d354aa7c6dd970bfbf7429f9d322b3f753dedf4e
KimJeongHyun <ptr1129@hanmail.net>
2021-12-24 14:40:36 +0900

    [Add] Mockup data 추가 & resize event 추가  - 각 Card는 해당 comp에 맞는 데이터를 json에서 가져온다.  - window width가 710보다 작거나 같으면 Card btn의 크기를 조정한다.

--- src/pages/Card/Card.js
+++ src/pages/Card/Card.js
@@ -1,28 +1,69 @@
+import React, { useState, useEffect } from 'react';
+
+import FetchUrl from '../../config/FetchUrl.json';
+
 import './card.scss';
-export default function Card({ eachCardData }) {
+
+const GET_CARD_COMMENT_DATA = FetchUrl.GET_CARD_COMMENT_DATA;
+
+export default function Card({ eachCardData, isWindowReduced }) {
+  const [cardContent, setCardContent] = useState([]);
+  const [inputCardContent, setInputCardContent] = useState('');
+
+  const isValid = inputCardContent.length > 0;
+
+  useEffect(() => {
+    fetch(GET_CARD_COMMENT_DATA)
+      .then(res => res.json())
+      .then(data => setCardContent(data[eachCardData.id]));
+  }, []);
+
+  function handleInput(event) {
+    const { value } = event.target;
+    setInputCardContent(value);
+  }
+
+  function addCardContent() {
+    const newContent = {
+      id: cardContent.length + 1,
+      content: inputCardContent,
+    };
+    setCardContent([...cardContent, newContent]);
+    setInputCardContent('');
+  }
+
+  function enterToAdd(event) {
+    event.preventDefault();
+    return event.key === 'Enter' && isValid ? addCardContent() : null;
+  }
+
   return (
     <article>
       <section className="CardArea">
         <header>{eachCardData.name}</header>
         <div className="CardInnerArea">
           <ul>
-            <li>asdf</li>
-            <li>asdf</li>
-            <li>asdf</li>
-            <li>asdf</li>
-            <li>asdf</li>
-            <li>asdf</li>
-            <li>asdf</li>
-            <li>asdf</li>
-            <li>asdf</li>
-            <li>asdf</li>
-            <li>asdf</li>
-            <li>asdf</li>
+            {cardContent
+              ? cardContent.map(item => <li key={item.id}>{item.content}</li>)
+              : ''}
           </ul>
         </div>
         <div className="inputBox">
-          <input type="text" />
-          <button>등록</button>
+          <input
+            type="text"
+            name="inputCardContent"
+            className={isWindowReduced ? 'reduce' : ''}
+            value={inputCardContent}
+            onChange={handleInput}
+            onKeyPress={enterToAdd}
+          />
+          <button
+            className={isWindowReduced ? 'reduce' : ''}
+            onClick={addCardContent}
+            disabled={!isValid}
+          >
+            등록
+          </button>
         </div>
       </section>
     </article>
--- src/pages/CardsContainer.js
+++ src/pages/CardsContainer.js
@@ -9,17 +9,34 @@ const GET_CARD_DATA = FetchUrl.GET_CARD_DATA;
 
 export default function CardContainer() {
   const [cards, setCards] = useState([]);
+  const [isWindowReduced, setIsWindowReduced] = useState();
+
   useEffect(() => {
     fetch(GET_CARD_DATA)
       .then(res => res.json())
       .then(data => setCards(data));
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+    function handleResize() {
+      window.resizeTo(710, 533);
+      window.innerWidth <= 710
+        ? setIsWindowReduced(true)
+        : setIsWindowReduced(false);
+    }
   }, []);
 
   return (
     <main>
       <div className="CardsContainer">
         {cards.map((item, index) => (
-          <Card key={index} eachCardData={item} />
+          <Card
+            key={index}
+            isWindowReduced={isWindowReduced}
+            eachCardData={item}
+          />
         ))}
       </div>
     </main>
